8f22eacbddedaac56676945b994221c5af6b41ee
Brian Nyawira <191464653+Obrienmaina-Mosbach@users.noreply.github.com>
2025-07-17 21:30:50 +0200

    db

--- server/config/db.js
+++ server/config/db.js
@@ -7,8 +7,6 @@ const connectDB = async () => {
     const conn = await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
-      // useFindAndModify: false, // Deprecated in Mongoose 6+
-      // useCreateIndex: true // Deprecated in Mongoose 6+
     });
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
